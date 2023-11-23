d1d5ebffad57844b92924fd9a28579df2de17b94
Akvile Jankauskaite <akvile.jankauskait@gmail.com>
2023-11-23 14:51:11 +0200

    Create migration for tables

---
+++ src/database/migrations/202311231400-createInitialTables.ts
@@ -0,0 +1,25 @@
+import { Kysely, SqliteDatabase } from 'kysely';
+
+export async function up(db: Kysely<SqliteDatabase>) {
+  await db.schema
+    .createTable('messages')
+    .ifNotExists()
+    .addColumn('id', 'integer', c => c.primaryKey().autoIncrement().notNull())
+    .addColumn('content', 'text', c => c.notNull())
+    .execute();
+
+  await db.schema
+    .createTable('sprints')
+    .ifNotExists()
+    .addColumn('code', 'text', c => c.notNull())
+    .addColumn('title', 'text', c => c.notNull())
+    .execute();
+
+    await db.schema
+    .createTable('congratulations')
+    .ifNotExists()
+    .addColumn('username', 'text', c => c.notNull())
+    .addColumn('messageId', 'text', c => c.references('messages.id').onDelete('cascade').notNull())
+    .addColumn('sprintCode', 'text', c => c.references('sprints.code').onDelete('cascade').notNull())
+    .execute();
+}
